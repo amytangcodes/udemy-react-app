a34d8884879da3c27b5b71f4c473a1367f0719e0
Amy Tang <amy@pathfactory.com>
2020-03-31 12:58:35 -0400

    separated projects into tabs menu

---
+++ src/seasons/SeasonsDisplay.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SeasonsDisplay = () => {
+  return (
+    <div >
+      <h1>Season Display</h1>
+    </div>
+  );
+};
+
+export default SeasonsDisplay;
---
+++ src/seasons/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import SeasonsDisplay from './SeasonsDisplay';
+
+export default class Seasons extends Component {
+  render() {
+    return (
+      <div>
+        <SeasonsDisplay />
+      </div>
+
+    )
+  }
+}
