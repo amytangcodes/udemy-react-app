50f31c4c42462b1c4c90d81b3589b1a46b0c5ec7
Amy Tang <amy@pathfactory.com>
2020-03-31 22:19:41 -0400

    render error message or lat and long

--- src/pageLayout/Menu.js
+++ src/pageLayout/Menu.js
@@ -4,8 +4,8 @@ import Section3 from '../section3';
 import Seasons from '../seasons';
 
 const panes = [
-  { menuItem: 'Section 3', render: () => <Tab.Pane attached='bottom'><Section3 /></Tab.Pane> },
-  { menuItem: 'Seasons', render: () => <Tab.Pane attached='bottom'><Seasons /></Tab.Pane> }
+  { menuItem: 'Section 4', render: () => <Tab.Pane attached='bottom'><Seasons /></Tab.Pane> },
+  { menuItem: 'Section 3', render: () => <Tab.Pane attached='bottom'><Section3 /></Tab.Pane> }
 ]
 
 export default () => <Tab menu={{ attached: 'top' }} panes={panes} />
--- src/seasons/index.js
+++ src/seasons/index.js
@@ -1,13 +1,46 @@
-import React, { Component } from 'react'
-import SeasonsDisplay from './SeasonsDisplay';
+import React from 'react';
 
-export default class Seasons extends Component {
+class Seasons extends React.Component {
+  // very first function that will be called before anything else
+  // so a good place to intialize state
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      lat: null,
+      long: null,
+      errorMessage: ""
+    }
+
+    window.navigator.geolocation.getCurrentPosition(
+      // a callback
+      position => {
+        this.setState({
+          lat: position.coords.latitude,
+          long: position.coords.longitude
+        });
+      },
+      // a failure callback
+      err => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
   render() {
+    const { lat, long, errorMessage } = this.state;
+    if (errorMessage) {
+      return <h3>{errorMessage}</h3>
+    }
+
     return (
       <div>
-        <SeasonsDisplay />
+        <h1>Seasons</h1>
+        <p>Latitude: {lat}</p>
+        <p>Longitude: {long}</p>
       </div>
-
-    )
+    );
   }
 }
+
+export default Seasons;
