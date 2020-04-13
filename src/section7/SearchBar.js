d1270b7215c3f7d391c732dc9f24cdb7a5aa1584
Amy <amytang77@gmail.com>
2020-04-13 16:44:15 -0400

    controlled vs uncontrolled component

--- src/section7/SearchBar.js
+++ src/section7/SearchBar.js
@@ -2,20 +2,27 @@ import React, { Component } from 'react'
 
 class SearchBar extends Component {
   state = {
-
-  }
-
-  onInputChange() {
-
+    text: "WWW."
   }
 
   render() {
+    const {text} = this.state;
     return (
       <div className="search-bar ui segment">
         <form action="" className="ui form">
           <div className="field">
             <label>Image Search</label>
-            <input type="text" placeholder="Search Bar" onChange={this.onInputChange}/>
+            <input
+              type="text"
+              placeholder="Search Bar"
+              value={text}
+              onChange={(e) => this.setState({
+                text: e.target.value.toUpperCase()
+              })}
+            />
+          </div>
+          <div className="field">
+            <p>Your input: <br/>{text}</p>
           </div>
         </form>
       </div>
