3557fc324b972903ee10e9a8ce8819aadac20b95
Amy <amytang77@gmail.com>
2020-04-13 23:13:22 -0400

    adding axios

--- src/section7/index.js
+++ src/section7/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
+import axios from 'axios';
 import Page from "../pageLayout/Page";
 import SearchBar from "./SearchBar";
 
 class Section7 extends React.Component {
   onSearchSubmit(text) {
-    console.log({text});
+    axios.get('https://api.unsplash.com/search/photos', {
+      params: { query: text },
+      headers: {
+        Authorization: 'Client-ID f7Ai5juTA6vfIYjMLfVE4lJ7PJyEBqqNuV-cQ_39Lw8'
+      }
+    });
   }
 
   render() {
