fcffdb26bca3740acc6d475fdc00a593ed4c29f0
Amy <amytang77@gmail.com>
2020-08-25 15:28:07 -0400

    linting fixes

--- src/project05/index.js
+++ src/project05/index.js
@@ -1,20 +1,20 @@
-import React from 'react';
-import Typography from '@material-ui/core/Typography';
+import React from "react";
+import Typography from "@material-ui/core/Typography";
 import Page from "../shared/Page";
-import ToDoForm from './ToDoForm';
-import ToDoList from './ToDoList';
-import useToDoState from './useToDoState';
+import ToDoForm from "./ToDoForm";
+import ToDoList from "./ToDoList";
+import useToDoState from "./useToDoState";
 
 const Index = () => {
   const { toDos, addToDo, deleteToDo } = useToDoState([]);
 
-  const handleSaveToDo = toDoText => {
+  const handleSaveToDo = (toDoText) => {
     const trimmedValue = toDoText.trim();
 
     if (trimmedValue.length > 0) {
       addToDo(trimmedValue);
     }
-  }
+  };
 
   return (
     <Page heading="React Hooks | A To Do App">
