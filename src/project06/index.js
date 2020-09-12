ada4a44fb07135869799d603c4267a9e2431be6d
Amy <amytang77@gmail.com>
2020-09-11 22:07:42 -0400

    set tabs for widget page

--- src/project06/index.js
+++ src/project06/index.js
@@ -4,27 +4,54 @@ import { dropdownData, accordianData } from "../api/projectSixData";
 import AccordionComponent from "./components/Accordion";
 import SearchComponent from "./components/Search";
 import DropdownComponent from "./components/Dropdown";
-import { Button } from "semantic-ui-react";
+import { Button, Tab } from "semantic-ui-react";
 
 const Index = () => {
   const [selected, setSelected] = useState(dropdownData[0]);
   const [showDropdown, setShowDropdown] = useState(true);
 
+  const panes = [
+    {
+      menuItem: "Dropdown",
+      render: () => {
+        return (
+          <Tab.Pane>
+            {showDropdown ? (
+              <DropdownComponent
+                selected={selected}
+                onSelectedChange={setSelected}
+                data={dropdownData}
+                initialText="Select Color"
+              />
+            ) : null}
+            <Button onClick={() => setShowDropdown(!showDropdown)}>
+              Toggle Dropdown
+            </Button>
+          </Tab.Pane>
+        );
+      },
+    },
+    {
+      menuItem: "Search",
+      render: () => (
+        <Tab.Pane>
+          <SearchComponent />
+        </Tab.Pane>
+      ),
+    },
+    {
+      menuItem: "Accordian",
+      render: () => (
+        <Tab.Pane>
+          <AccordionComponent items={accordianData} />;
+        </Tab.Pane>
+      ),
+    },
+  ];
+
   return (
     <Page heading="React Hooks | Widgets App">
-      <Button onClick={() => setShowDropdown(!showDropdown)}>
-        Toggle Dropdown
-      </Button>
-      {showDropdown ? (
-        <DropdownComponent
-          selected={selected}
-          onSelectedChange={setSelected}
-          data={dropdownData}
-          initialText="Select Color"
-        />
-      ) : null}
-      <SearchComponent />
-      <AccordionComponent items={accordianData} />
+      <Tab panes={panes} />
     </Page>
   );
 };
